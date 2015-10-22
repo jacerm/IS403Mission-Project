5b3e58230cf9ec4b8d1be702063e0c90a38fa2d9
Jace <jace.milne@gmail.com>
2015-10-22 12:34:19 -0600

    First commit to github

---
+++ Project1MissionSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project1MissionSite.Startup))]
+namespace Project1MissionSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
